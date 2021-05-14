2d18a0f2d08b328e2e961939b024743e71561b99
Arjun <arjunptb206@gmail.com>
2021-05-14 13:18:17 +0530

    binding view and component binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,10 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  aim = "Your Perfect Banking Partner";
+  acno = "account number pls";
+  pswd = "";
+  accountDetails = {
+    1000: { acno: 1000, username: "userone", password: "1000", balance: 50000 },
+    1001: { acno: 1001, username: "usertwo", password: "usertwo", balance: 5000 },
+    1002: { acno: 1002, username: "userthree", password: "userthree", balance: 10000 },
+    1003: { acno: 1003, username: "userfour", password: "userfour", balance: 6000 }
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  // accnoChange(event: any) {
+  //   this.accno = event.target.value;
+  //   console.log(this.accno);
 
+  // }
+  // pwdChange(event: any) {
+  //   this.pwd = event.target.value;
+  //   console.log(this.pwd);
+  // }
+
+  login(a:any,p:any) {
+   // alert("login clicked")
+    var accno = a.value;
+    var pwd=p.value
+    let users=this.accountDetails;
+    if (accno in users) {
+        if(pwd==users[accno]["password"]){
+          alert("login succesfull");
+        }
+    
+  else{
+      alert("incorrect password");
+  }
+}
+else{
+  alert("invalid account no");
+}
+
+}
 }
